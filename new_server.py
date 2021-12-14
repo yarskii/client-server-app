83a2b046da388cd3fdbb0a0ea53e4379119eb749
yarskii <71975048+yarskii@users.noreply.github.com>
2021-12-14 18:14:37 +0300

    lesson_8

--- new_server.py
+++ new_server.py
@@ -1,66 +1,130 @@
+import argparse
 import json
 import logging
+import select
 import socket
 import sys
+import time
 
 from utils import get_msg, send_msg
 import logs.server_log_config
+from decorators import log
 
 SERVER_LOGGER = logging.getLogger('server')
 
 
-def client_message_handler(message):
+@log
+def client_message_handler(message, messages_list, client, clients, names):
+    SERVER_LOGGER.debug(f'Parsing a message from a client: {message}')
     if 'action' in message and message['action'] == 'presence' and 'time' in message \
-            and 'user' in message and message['user']['account_name'] == 'User':
-        return {'response': 200}
-    return {
-        'respondefault_ip_addresse': 400,
-        "error": 'Wrong Request'
-    }
+            and 'user' in message:
+        if message['user']['account_name'] not in names.keys():
+            names[message['user']['account_name']] = client
+            send_msg(client, {'response: 200'})
+        else:
+            response = {'response': 400, 'error': 'The username is already taken.'}
+            send_msg(client, response)
+            clients.remove(client)
+            client.close()
+        return
+    elif 'action' in message and message['action'] == 'message' and \
+            'time' in message and 'message_text' in message and \
+            'destination' in message and 'sender' in message:
+        messages_list.append(message)
+        return
+    elif 'action' in message and message['action'] == 'exit' and 'account_name' in message:
+        clients.remove(names[message["account_name"]])
+        names[message['account_name']].close()
+        del names[message['account_name']]
+        return
+    else:
+        response = {'response': 400, 'error': 'The request is invalid.'}
+        send_msg(client, response)
+        return
+
+
+@log
+def process_message(message, names, listen_socks):
+    if message['destination'] in names and names[message['destination']] in listen_socks:
+        send_msg(names[message['destination']], message)
+        SERVER_LOGGER.info(f'Message sent to user {message["destination"]} '
+                           f'from user {message["sender"]}.')
+    elif message['destination'] in names and names[message['destination']] not in listen_socks:
+        raise ConnectionError
+    else:
+        SERVER_LOGGER.error(
+            f'User {message["destination"]} not registered on the server, '
+            f'sending the message is not possible.')
+
+
+@log
+def arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', default=7771, type=int, nargs='?')
+    parser.add_argument('-a', default='', nargs='?')
+    namespace = parser.parse_args(sys.argv[1:])
+    address = namespace.a
+    port = namespace.p
+
+    if not 1023 < port < 65536:
+        SERVER_LOGGER.critical(
+            f'Attempting to start the server with an invalid port '
+            f'{port}. Valid addresses are from 1024 to 65535.')
+        sys.exit(1)
+
+    return address, port
 
 
 def main():
-    try:
-        if '-p' in sys.argv:
-            port = int(sys.argv[sys.argv.index('-p') + 1])
-        else:
-            port = 7776
-        if port < 1024 or port > 65535:
-            raise ValueError
-    except IndexError:
-        SERVER_LOGGER.error('After the -\'p\' parameter, you must specify the port number.')
-        sys.exit(1)
-    except ValueError:
-        SERVER_LOGGER.critical('Only a number in the range from 1024 to 65535 can be specified as a port.')
-        sys.exit(1)
-
-    try:
-        if '-a' in sys.argv:
-            address = sys.argv[sys.argv.index('-a') + 1]
-        else:
-            address = ''
-    except IndexError:
-        SERVER_LOGGER.error('After the \'a\' - you must specify the address that the server will listen to.')
-        sys.exit(1)
+    address, port = arg_parser()
 
     transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     transport.bind((address, port))
+    transport.settimeout(0.5)
+
+    clients = []
+    messages = []
+
+    names = dict()
 
     transport.listen(5)
 
     while True:
-        client, client_address = transport.accept()
         try:
-            msg_from_client = get_msg(client)
-            SERVER_LOGGER.debug(f'Message received {msg_from_client}')
-            response = client_message_handler(msg_from_client)
-            SERVER_LOGGER.info(f'Formed a response to the client {response}')
-            send_msg(client, response)
-            SERVER_LOGGER.debug(f'Client connection {client_address} is closing.')
-            client.close()
-        except (ValueError, json.JSONDecodeError):
-            SERVER_LOGGER.error('Invalid message received from client.')
-            client.close()
+            client, client_address = transport.accept()
+        except OSError:
+            pass
+        else:
+            SERVER_LOGGER.info(f'PC connection established {client_address}')
+            clients.append(client)
+
+        recv_data_lst = []
+        send_data_lst = []
+        err_lst = []
+        try:
+            if clients:
+                recv_data_lst, send_data_lst, err_lst = select.select(clients, clients, [], 0)
+        except OSError:
+            pass
+
+        if recv_data_lst:
+            for client_with_message in recv_data_lst:
+                try:
+                    client_message_handler(get_msg(client_with_message),
+                                           messages, client_with_message, clients, names)
+                except Exception:
+                    SERVER_LOGGER.info(f'Client {client_with_message.getpeername()} '
+                                       f'disconnected from the server.')
+                    clients.remove(client_with_message)
+
+        for i in messages:
+            try:
+                process_message(i, names, send_data_lst)
+            except Exception:
+                SERVER_LOGGER.info(f'Customer contact with name {i["destination"]} was lost')
+                clients.remove(names[i['destination']])
+                del names[i['destination']]
+        messages.clear()
 
 
 if __name__ == '__main__':
